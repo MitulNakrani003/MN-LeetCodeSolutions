9a77cabcf4fa214bb09db109b42c788b32308c79
Mitul Nakrani <106060775+MitulNakrani003@users.noreply.github.com>
2025-10-26 22:28:10 -0400

    Medium in .py - Date: 10-26-2025

---
+++ LeetCode/Medium/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal-10-26-2025-22-28-10.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root == None:
+            return []
+        levels = [[root]]
+        answerlevels = [[root.val]]
+        i = 0
+        while True:
+            thislevel = []
+            vallevels = []
+            for x in levels[i]:
+                if x.right:
+                    thislevel.append(x.left)
+                    vallevels.append(x.left.val)
+                if x.left:
+                    thislevel.append(x.right)
+                    vallevels.append(x.right.val)
+            if len(thislevel) == 0:
+                break
+            levels.append(thislevel)
+            answerlevels.append(vallevels)
+            i+=1
+        return answerlevels
+
