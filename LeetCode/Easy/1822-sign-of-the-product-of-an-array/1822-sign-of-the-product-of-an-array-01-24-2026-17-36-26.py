db81f1c317a9b2d6eb4a6691ad67cf4541b53f8d
Mitul Nakrani <106060775+MitulNakrani003@users.noreply.github.com>
2026-01-24 17:36:26 -0500

    Easy in .py - Date: 01-24-2026

---
+++ LeetCode/Easy/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array-01-24-2026-17-36-26.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        result = 1
+        for x in nums:
+            if x < 0:
+                result*=(-1)
+            elif x > 0:
+                result*=1
+            else:
+                return 0
+        return result
