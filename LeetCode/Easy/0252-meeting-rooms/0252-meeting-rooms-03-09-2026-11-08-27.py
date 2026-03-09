a9cf98890e1141bbdf5494c80221fc3e4f876d74
Mitul Nakrani <106060775+MitulNakrani003@users.noreply.github.com>
2026-03-09 11:08:27 -0400

    Easy in .py - Date: 03-09-2026

---
+++ LeetCode/Easy/0252-meeting-rooms/0252-meeting-rooms-03-09-2026-11-08-27.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) == 0:
+            return True
+        intervals.sort()
+        for i in range(1,len(intervals)):
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
+        return True
+
+        
